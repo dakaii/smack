023434c1858c47c5e80494f17ca11a9002c2252f
dakaii <daiki815@gmail.com>
2018-01-04 12:46:20 +0900

    asjdlf

--- app/src/main/java/dakaii/smack/Utils/Constants.kt
+++ app/src/main/java/dakaii/smack/Utils/Constants.kt
@@ -6,3 +6,4 @@ package dakaii.smack.Utils
 
 const val BASE_URL = "https://udemychatapi.herokuapp.com/v1/"
 const val URL_REGISTER = "${BASE_URL}account/register"
+const val URL_LOGIN = "${BASE_URL}account/login"
